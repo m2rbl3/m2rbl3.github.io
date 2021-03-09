49e13d5373ee5745a385c21a525f8772dd5b7465
li_zi_heng <liziheng@zuzuche.com>
2021-03-09 20:13:10 +0800

    feat: 只保留编译后文件

--- src/components/PersonalInfo/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ContactInfo } from './ContactInfo';
-export { default as Education } from './Education';
-export { default as PersonalRecognition } from './PersonalRecognition';
-export { default as PersonalInfo } from './PersonalInfo';
